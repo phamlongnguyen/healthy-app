6db39b32ebf064b5a1eb9f336f75121bfca50558
ext-nguyen.pham@spdigital.sg <24faccdc16bd0d96aec54bbd6fbec1c88786df34>
2024-11-11 20:07:15 +0700

    build: add initial configuration and packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+        api: 'modern-compiler',
+      },
+    },
+  },
 })
