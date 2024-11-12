4071664e78b4727f42fcf4c993f0c3d316b72294
NguyenPham <phamlongnguyen97@gmail.com>
2024-11-12 20:00:29 +0700

    feat: Update page Column with api mock

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,105 +1,55 @@
-import { format, parseISO } from 'date-fns'
-import Column1Jpg from '../../assets/images/column-1.jpg'
-import Column2Jpg from '../../assets/images/column-2.jpg'
-import Column3Jpg from '../../assets/images/column-3.jpg'
-import Column4Jpg from '../../assets/images/column-4.jpg'
-import Column5Jpg from '../../assets/images/column-5.jpg'
-import Column6Jpg from '../../assets/images/column-6.jpg'
-import Column7Jpg from '../../assets/images/column-7.jpg'
-import Column8Jpg from '../../assets/images/column-8.jpg'
-import './styles.scss'
+import { format, parseISO } from 'date-fns';
 
-const MOCK_DATA_TOPICS = [
-  {
-    id: 1,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column1Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 2,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column2Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 3,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column3Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 4,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column4Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 5,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column5Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 6,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column6Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 7,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column7Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-  {
-    id: 8,
-    title:
-      '魚を食べて頭もカラダも元気に！知っておきたい魚を食べるメリ 魚を食べるメリ',
-    tags: ['魚料理', ' 和食', 'DHA'],
-    image: Column8Jpg,
-    timestamp: '2021-05-17T14:43:11.551Z',
-  },
-]
+import './styles.scss';
+import useFetchDataWithLoadMore from '../../hooks/useFetchDataWithLoadMore';
+import { getTopicsData } from '../../services';
+import { ButtonGradient } from '../common';
+import { CircularProgress } from '@mui/material';
 
 const Topics = () => {
+  const { isLoading, isLoadingButton, data, loadMore } =
+    useFetchDataWithLoadMore({ funcFetchData: getTopicsData });
+
   return (
     <div className="topics">
-      {MOCK_DATA_TOPICS.map(topic => {
-        return (
-          <div className="topics__item" key={topic.id}>
-            <div
-              style={{ backgroundImage: `url(${topic.image})` }}
-              className="topics__item__img"
-            >
-              <p>{format(parseISO(topic.timestamp), 'yyyy.MM.dd  HH:mm')}</p>
-            </div>
+      {isLoading ? (
+        <div className="topics__loading">
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className="topics__body">
+          {data.map((topic, index) => {
+            return (
+              <div className="topics__body__item" key={topic.id + index}>
+                <div
+                  style={{ backgroundImage: `url(${topic.image})` }}
+                  className="topics__body__item__img"
+                >
+                  <p>
+                    {format(parseISO(topic.timestamp), 'yyyy.MM.dd  HH:mm')}
+                  </p>
+                </div>
 
-            <p className="topics__item__title">{topic.title}</p>
-            <div className="topics__item__tags">
-              {topic.tags.map(tag => (
-                <span key={tag + topic.id}>#{tag}</span>
-              ))}
-            </div>
-          </div>
-        )
-      })}
+                <p className="topics__body__item__title">{topic.title}</p>
+                <div className="topics__body__item__tags">
+                  {topic.tags.map(tag => (
+                    <span key={tag + topic.id}>#{tag}</span>
+                  ))}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="my-diary__button">
+        <ButtonGradient
+          label="コラムをもっと見る"
+          onClick={loadMore}
+          loading={isLoadingButton}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topics
+export default Topics;
--- src/pages/ColumnPage/ColumnPage.jsx
+++ src/pages/ColumnPage/ColumnPage.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
-import { ButtonGradient } from '../../components/common'
-import './styles.scss'
-import { Recommended, Topics } from '../../components'
+import { Recommended, Topics } from '../../components';
+import './styles.scss';
 
 const ColumnPage = () => {
   return (
     <div className="column-page">
       <Recommended />
       <Topics />
-      <div className="column-page__button">
-        <ButtonGradient label="コラムをもっと見る" />
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ColumnPage
+export default ColumnPage;
