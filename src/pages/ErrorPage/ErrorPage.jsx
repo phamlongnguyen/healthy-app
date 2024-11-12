bbb265a5c48c3f417e6df8e92e1696b75c02047d
NguyenPham <phamlongnguyen97@gmail.com>
2024-11-12 19:33:35 +0700

    feat: update UI error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,16 +1,23 @@
-import { useRouteError } from 'react-router-dom'
-
+import { useNavigate, useRouteError } from 'react-router-dom';
+import './styles.scss';
+import { Button } from '@mui/material';
 export default function ErrorPage() {
-  const error = useRouteError()
-  console.error(error)
+  const navigate = useNavigate();
 
+  const error = useRouteError();
+  const goBack = () => {
+    navigate('/');
+  };
   return (
-    <div id="error-page">
+    <div className="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <Button variant="contained" onClick={goBack}>
+        Back to Home
+      </Button>
     </div>
-  )
+  );
 }
