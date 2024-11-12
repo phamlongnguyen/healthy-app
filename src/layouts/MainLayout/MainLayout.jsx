7449f9b624e7854d5006ffc4dfa4a07d92dc47da
NguyenPham <phamlongnguyen97@gmail.com>
2024-11-12 19:31:05 +0700

    feat: Add button scroll top

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,15 +1,25 @@
-import { Outlet } from 'react-router-dom'
-import './styles.scss'
-import { HeaderPage, FooterPage } from '../../components'
-
+import { Outlet } from 'react-router-dom';
+import './styles.scss';
+import { HeaderPage, FooterPage } from '../../components';
+import { IconArrowUp } from '../../assets/icons';
 const MainLayout = () => {
   return (
     <div className="main-layout">
       <HeaderPage />
-      <Outlet />
+      <div className="main-layout__body">
+        <Outlet />
+        <div className="main-layout__body__top">
+          <button
+            className="main-layout__body__top__btn"
+            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          >
+            <IconArrowUp />
+          </button>
+        </div>
+      </div>
       <FooterPage />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
